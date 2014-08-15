51d512bf84a32129947e508077a22ac695b9ee85
Gilbert Jeiziner <gjn@space.ch>
2014-08-15 14:48:02 +0200

    Remove globals from api loader

--- chsdi/templates/loader.js
+++ chsdi/templates/loader.js
@@ -5,19 +5,14 @@ from chsdi.lib.helpers import versioned
 
 lang = pageargs['lang']
 mode = pageargs['mode']
-data = pageargs['data']
+data = pageargs['data'].replace('\n', '').replace('\r', '').replace(' ', '').replace('\'', '&quot;')
+serviceurl = pageargs['serviceurl']
 
-layersconfig = """window.GeoAdmin.getConfig  = function(){ return %s } """ % data
 %>
 (function() {
-if (typeof window['GeoAdmin'] == 'undefined') window.GeoAdmin = {};
-window.GeoAdmin.lang = "${lang}";
-
-${layersconfig|n}
 // Load css
 document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('chsdi:static/css/ga.css'))}" + '" />');
 // Load js
-document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/serverconfig.js'))}" +  '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG21781.js'))}" + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/EPSG2056.js'))}" + '"></scr' + 'ipt>');
@@ -30,5 +25,11 @@ document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(requ
 % else:
 document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('chsdi:static/js/ga.js'))}" + '"></scr' + 'ipt>');
 % endif
+// Setting language
+document.write('<scr' + 'ipt type="text/javascript">ga.Lang.setCode("${lang|n}");</scr' + 'ipt>');
+// Setting service Url
+document.write('<scr' + 'ipt type="text/javascript">ga.setServiceUrl("${serviceurl|n}");</scr' + 'ipt>');
+// Setting Configuration
+document.write('<scr' + 'ipt type="text/javascript">ga.layer.setConfig(${data|n});</scr' + 'ipt>');
 })();
 
