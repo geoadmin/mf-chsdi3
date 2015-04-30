5d1de1634416dd05f5ac17743e8e0700ee38b0a3
Bruno Friedmann <bruno@ioda-net.ch>
2015-04-30 16:28:01 +0200

    Filter empty debug_header

--- chsdi/lib/helpers.py
+++ chsdi/lib/helpers.py
@@ -229,7 +229,7 @@ def transformCoordinate(wkt, srid_from, srid_to):
 
 
 def get_debug_headers(config):
-    if 'pyramid.debug_header' in config:
+    if 'pyramid.debug_header' in config and config['pyramid.debug_header'].strip() :
         raw_headers = config['pyramid.debug_header']
         headers_list = [header.split(':') for header in raw_headers.split(';')]
         return {name.strip(): value.strip() for name, value in headers_list}
