c895e58e693f71dd69b916b9f1c8b1420ed14f62
loicgasser <loicgasser4@gmail.com>
2016-03-01 17:30:31 +0100

    Make use of gatilegrid module

--- chsdi/models/bod.py
+++ chsdi/models/bod.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from gatilegrid import GeoadminTileGrid
 from sqlalchemy import Column, Text, Integer, Boolean, DateTime
 from sqlalchemy.dialects import postgresql
 
@@ -146,13 +147,9 @@ class LayersConfig(Base):
         return self.__dict__['geojsonUrl%s' % lang]
 
     def _getResolutionsFromMatrixSet(self, matrixSet):
-        resolutions = [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250,
-                       2000, 1750, 1500, 1250, 1000, 750, 650, 500,
-                       250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5,
-                       0.25, 0.1]
-
+        gagrid = GeoadminTileGrid()
         matrixSet = int(matrixSet.split('_')[1])
-        return resolutions[0:matrixSet + 1]
+        return gagrid.RESOLUTIONS[0:matrixSet + 1]
 
 
 class BodLayerDe(Base, Bod):
@@ -320,63 +317,46 @@ class ServiceMetadataEn(Base, ServiceMetadata):
     __tablename__ = 'view_wms_service_metadata_en'
     __table_args__ = ({'schema': 're3', 'autoload': False})
 
-# TODO use GetCap model to fill that up instead
-
 
 def computeHeader(mapName):
+    gagrid = GeoadminTileGrid()
+    minZoom = 0
+    maxZoom = 28
+    lods = []
+    for zoom in range(minZoom, maxZoom + 1):
+        lods.append(
+            {'level': zoom,
+             'resolution': gagrid.getResolution(zoom),
+             'scale': gagrid.getScale(zoom),
+             'width': gagrid.numberOfXTilesAtZoom(zoom),
+             'height': gagrid.numberOfYTilesAtZoom(zoom)}
+        )
+
     return {
         'mapName': mapName,
         'description': 'Configuration for the map (topic) ' + mapName,
         'copyrightText': 'Data ' + mapName,
         'layers': [],
-        'spatialReference': {"wkid": 21781},
+        'spatialReference': {"wkid": gagrid.spatialReference},
         'tileInfo': {
-            'rows': 256,  # tile width in pixel
-            'cols': 256,
+            'rows': gagrid.tileSizePx,  # tile width in pixel
+            'cols': gagrid.tileSizePx,
             'dpi': 96,
             'format': 'PNG,JPEG',
-            'compressionQuality': '',
-            'origin': {"x": 420000, "y": 350000, "spatialReference": {"wkid": 21781}},
-            'spatialReference': {"wkid": 21781},
-            'lods': [
-                {'level': 0, 'resolution': 4000, 'scale': 14285750.5715, 'width': 1, 'height': 1},
-                {'level': 1, 'resolution': 3750, 'scale': 13392891.1608, 'width': 1, 'height': 1},
-                {'level': 2, 'resolution': 3500, 'scale': 12500031.7501, 'width': 1, 'height': 1},
-                {'level': 3, 'resolution': 3250, 'scale': 11607172.3393, 'width': 1, 'height': 1},
-                {'level': 4, 'resolution': 3000, 'scale': 10714312.9286, 'width': 1, 'height': 1},
-                {'level': 5, 'resolution': 2750, 'scale': 9821453.51791, 'width': 1, 'height': 1},
-                {'level': 6, 'resolution': 2500, 'scale': 8928594.10719, 'width': 1, 'height': 1},
-                {'level': 7, 'resolution': 2250, 'scale': 8035734.69647, 'width': 1, 'height': 1},
-                {'level': 8, 'resolution': 2000, 'scale': 7142875.28575, 'width': 1, 'height': 1},
-                {'level': 9, 'resolution': 1750, 'scale': 6250015.87503, 'width': 2, 'height': 1},
-                {'level': 10, 'resolution': 1500, 'scale': 5357156.46431, 'width': 2, 'height': 1},
-                {'level': 11, 'resolution': 1250, 'scale': 4464297.05359, 'width': 2, 'height': 1},
-                {'level': 12, 'resolution': 1000, 'scale': 3571437.64288, 'width': 2, 'height': 2},
-                {'level': 13, 'resolution': 750, 'scale': 2678578.23216, 'width': 3, 'height': 2},
-                {'level': 14, 'resolution': 650, 'scale': 2321434.46787, 'width': 3, 'height': 2},
-                {'level': 15, 'resolution': 500, 'scale': 1785718.82144, 'width': 4, 'height': 3},
-                {'level': 16, 'resolution': 250, 'scale': 892859.410719, 'width': 8, 'height': 5},
-                {'level': 17, 'resolution': 100, 'scale': 357143.764288, 'width': 19, 'height': 13},
-                {'level': 18, 'resolution': 50, 'scale': 178571.882144, 'width': 38, 'height': 25},
-                {'level': 19, 'resolution': 20, 'scale': 71428.7528575, 'width': 94, 'height': 63},
-                {'level': 20, 'resolution': 10, 'scale': 35714.3764288, 'width': 188, 'height': 125},
-                {'level': 21, 'resolution': 5, 'scale': 17857.1882144, 'width': 375, 'height': 250},
-                {'level': 22, 'resolution': 2.5, 'scale': 8928.59410719, 'width': 750, 'height': 500},
-                {'level': 23, 'resolution': 2, 'scale': 7142.87528575, 'width': 938, 'height': 625},
-                {'level': 24, 'resolution': 1.5, 'scale': 5357.15646431, 'width': 1250, 'height': 834},
-                {'level': 25, 'resolution': 1, 'scale': 3571.43764288, 'width': 1875, 'height': 1250},
-                {'level': 26, 'resolution': 0.5, 'scale': 1785.71882144, 'width': 3750, 'height': 2500},
-                {'level': 27, 'resolution': 0.25, 'scale': 892.857, 'width': 7500, 'height': 5000},
-                {'level': 28, 'resolution': 0.1, 'scale': 357.1425, 'width': 18750, 'height': 12500}
-            ]
+            'compressionQuality': None,
+            'origin': {'x': gagrid.origin[0], 'y': gagrid.origin[1],
+                       'spatialReference': {'wkid': gagrid.spatialReference}},
+            'spatialReference': {'wkid': 21781},
+            'lods': lods
         },
         'initialExtent': {
             'xmin': 458000, 'ymin': 76375, 'xmax': 862500, 'ymax': 289125,
-            'spatialReference': {'wkid': 21781}
+            'spatialReference': {'wkid': gagrid.spatialReference}
         },
         'fullExtent': {
-            'xmin': 42000, 'ymin': 30000, 'xmax': 900000, 'ymax': 350000,
-            'spatialReference': {'wkid': 21781}
+            'xmin': gagrid.extent[0], 'ymin': gagrid.extent[1],
+            'xmax': gagrid.extent[2], 'ymax': gagrid.extent[3],
+            'spatialReference': {'wkid': gagrid.spatialReference}
         },
         'units': 'esriMeters',
         'capabilities': 'Map'
