9b4e74992a6f61f22bbdfc56e79c755b7708a9aa
kom <mariavkourouni@gmail.com>
2016-07-22 12:08:49 +0200

    Improve coverage owschecker

--- chsdi/tests/integration/test_owschecker.py
+++ chsdi/tests/integration/test_owschecker.py
@@ -2,6 +2,8 @@
 
 from chsdi.tests.integration import TestsBase
 from nose.plugins.capture import Capture
+from chsdi.views import owschecker as ows
+from chsdi.views.owschecker import to_bunch
 
 
 class TestOwsChecker(TestsBase):
@@ -14,6 +16,25 @@ class TestOwsChecker(TestsBase):
     def tearDown(self):
         super(TestOwsChecker, self).tearDown()
         del self.capture
+
+    def _callFUT(self):
+        base_url = 'http://wms.geo.admin.ch'
+        dictionary = {'service': 'WMS', 'base_url': base_url}
+        bunch = ows.Bunch(dictionary)
+        return bunch
+
+    def test_bunch(self):
+        bunch = self._callFUT()
+        self.assertEqual({'base_url': 'http://wms.geo.admin.ch', 'service': 'WMS'}, bunch)
+
+    def test_to_bunch(self):
+        dictionary_1 = {'base_url': 'http://wms.geo.admin.ch', 'service': 'WMS'}
+        bunch_1 = to_bunch(dictionary_1)
+        self.assertEqual(bunch_1, {'base_url': 'http://wms.geo.admin.ch', 'service': 'WMS'})
+
+        dictionary_2 = {'base_url': 'http://wms.geo.admin.ch', 'service': {'service1': 'WMS', 'service2': 'WMTS'}}
+        bunch_2 = to_bunch(dictionary_2)
+        self.assertEqual(bunch_2, {'base_url': 'http://wms.geo.admin.ch', 'service': {'service2': 'WMTS', 'service1': 'WMS'}})
 
     def test_bykvp_no_args(self):
         self.testapp.get('/owschecker/bykvp', status=400)
@@ -40,3 +61,13 @@ class TestOwsChecker(TestsBase):
         resp = self.testapp.get('/owschecker/bykvp', params={'service': 'WFS', 'base_url': base_url}, status=200)
         self.assertEqual(resp.content_type, 'application/json')
         resp.mustcontain("Checked Service: WFS")
+
+    def test_bykvp_wms_request_restful(self):
+        base_url = 'http://wms.geo.admin.ch'
+        resp = self.testapp.get('/owschecker/bykvp', params={'service': 'WMS', 'base_url': base_url, 'restful': True}, status=200)
+        self.assertEqual(resp.content_type, 'application/json')
+
+    def test_form_params(self):
+        base_url = 'http://wms.geo.admin.ch'
+        resp = self.testapp.get('/owschecker/form', params={'service': 'WMS', 'base_url': base_url}, status=200)
+        self.assertEqual(resp.content_type, 'text/html')
