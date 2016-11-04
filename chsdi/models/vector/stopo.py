a5677e77544d23bb27d1eaf2e7f43429038e31e8
daguer <daguerdat@gmail.com>
2016-11-04 09:11:04 +0100

    Rename shop layers

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -205,8 +205,16 @@ class GravimetrischerAtlasMetadata (Base, ShopProductClass, Vector):
     __bodId__ = 'ch.swisstopo.geologie-gravimetrischer_atlas.metadata'
     id = Column('s_map_number', Integer, primary_key=True)
 
-
 register('ch.swisstopo.geologie-gravimetrischer_atlas.metadata', GravimetrischerAtlasMetadata)
+
+
+class GravimetrischerAtlasPapierMetadata (Base, ShopProductClass, Vector):
+    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'view_gridstand_gravimetrie_atlas_metadata_shop'
+    __bodId__ = 'ch.swisstopo.geologie-gravimetrischer_atlas_papier.metadata'
+    id = Column('s_map_number', Integer, primary_key=True)
+
+register('ch.swisstopo.geologie-gravimetrischer_atlas_papier.metadata', GravimetrischerAtlasPapierMetadata)
 
 
 class Generalkarte300Metadata(Base, ShopProductClass, Vector):
@@ -231,6 +239,14 @@ class Landeskarte1000Metadata(Base, ShopProductClass, Vector):
     __bodId__ = 'ch.swisstopo.lk1000-papierkarte.metadata'
 
 register('ch.swisstopo.lk1000-papierkarte.metadata', Landeskarte1000Metadata)
+
+
+class Landeskarte1000PapierMetadata(Base, ShopProductClass, Vector):
+    __table_args__ = ({'schema': 'datenstand', 'autoload': False})
+    __tablename__ = 'view_gridstand_lk1000'
+    __bodId__ = 'ch.swisstopo.landeskarte1000_papier.metadata'
+
+register('ch.swisstopo.landeskarte1000_papier.metadata', Landeskarte1000PapierMetadata)
 
 
 class SegelFlug300Metadata(Base, ShopProductClass, Vector):
@@ -288,6 +304,14 @@ class Icao500Metadata(Base, ShopProductClass, Vector):
     __bodId__ = 'ch.swisstopo.icao-papierkarte.metadata'
 
 register('ch.swisstopo.icao-papierkarte.metadata', Icao500Metadata)
+
+
+class GrundwasserVulnerabilitaetPapierMetadata(Base, ShopProductClass, Vector):
+    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'view_gridstand_gkwvul500'
+    __bodId__ = 'ch.swisstopo.geologie-grundwasservulnerabilitaet_papier.metadata'
+
+register('ch.swisstopo.geologie-grundwasservulnerabilitaet_papier.metadata', GrundwasserVulnerabilitaetPapierMetadata)
 
 
 class GrundwasserVulnerabilitaetMetadata(Base, ShopProductClass, Vector):
