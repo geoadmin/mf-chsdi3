f44cfd000734233b261c59c28d930237596267d0
Tobias Reber <toby@seidenfein.ch>
2016-11-09 11:35:05 +0100

    added 4 oereb layers

--- chsdi/models/vector/oereb/uvek.py
+++ chsdi/models/vector/oereb/uvek.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, Text
+from sqlalchemy import Column, Unicode
 from geoalchemy2.types import Geometry
 
 from chsdi.models import register_oereb, bases
@@ -9,61 +9,78 @@ from chsdi.models.vector import Vector
 Base = bases['uvek']
 
 
-class Projektierungszonen_Oereb(Base, Vector):
-    __tablename__ = 'projektierungszonen_oereb'
-    __table_args__ = ({'schema': 'bazl', 'autoload': False})
-    __bodId__ = 'ch.bazl.projektierungszonen-flughafenanlagen.oereb'
-    id = Column('stabil_id', Text, primary_key=True)
+class OerebBase:
+    id = Column('stabil_id', Unicode, primary_key=True)
     the_geom = Column(Geometry(geometry_type='GEOMETRY',
                                dimension=2, srid=21781))
-    geomType = Column('geom_type', Text)
-    xmlData = Column('xml_data', Text)
-    bgdi_created = Column('bgdi_created', Text)
-    data_created = Column('data_created', Text)
-
-register_oereb('ch.bazl.projektierungszonen-flughafenanlagen.oereb', Projektierungszonen_Oereb)
+    geomType = Column('geom_type', Unicode)
+    xmlData = Column('xml_data', Unicode)
+    bgdi_created = Column('bgdi_created', Unicode)
+    data_created = Column('data_created', Unicode)
 
 
-class Sichereitszonen_Oereb(Base, Vector):
-    __tablename__ = 'sichereitszonen_oereb'
-    __table_args__ = ({'schema': 'bazl', 'autoload': False})
-    __bodId__ = 'ch.bazl.sicherheitszonenplan.oereb'
-    id = Column('stabil_id', Text, primary_key=True)
-    the_geom = Column(Geometry(geometry_type='GEOMETRY',
-                               dimension=2, srid=21781))
-    geomType = Column('geom_type', Text)
-    xmlData = Column('xml_data', Text)
-    bgdi_created = Column('bgdi_created', Text)
-    data_created = Column('data_created', Text)
+# ASTRA
+class ProjektierungszonenNationalstrassenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'projektierungszonen_nationalstrassen_oereb'
+    __table_args__ = ({'schema': 'astra', 'autoload': False})
+    __bodId__ = 'ch.astra.projektierungszonen-nationalstrassen.oereb'
 
-register_oereb('ch.bazl.sicherheitszonenplan.oereb', Sichereitszonen_Oereb)
+register_oereb(ProjektierungszonenNationalstrassenOereb.__bodId__, ProjektierungszonenNationalstrassenOereb)
 
 
-class Kataster_belasteten_standorte_oev_Oereb(Base, Vector):
+# BAV
+class KatasterBelastetenStandorteOevOereb(Base, OerebBase, Vector):
     __tablename__ = 'kataster_belasteter_standorte_oev_oereb'
     __table_args__ = ({'schema': 'bav', 'autoload': False})
     __bodId__ = 'ch.bav.kataster-belasteter-standorte-oev.oereb'
-    id = Column('stabil_id', Text, primary_key=True)
-    the_geom = Column(Geometry(geometry_type='GEOMETRY',
-                               dimension=2, srid=21781))
-    geomType = Column('geom_type', Text)
-    xmlData = Column('xml_data', Text)
-    bgdi_created = Column('bgdi_created', Text)
-    data_created = Column('data_created', Text)
 
-register_oereb('ch.bav.kataster-belasteter-standorte-oev.oereb', Kataster_belasteten_standorte_oev_Oereb)
+register_oereb(KatasterBelastetenStandorteOevOereb.__bodId__, KatasterBelastetenStandorteOevOereb)
 
 
-class Kataster_belasteten_standorte_zivflpl_Oereb(Base, Vector):
+class ProjektierungszonenEisenbahnanlagenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'projektierungszonen_eisenbahnanlagen_oereb'
+    __table_args__ = ({'schema': 'bav', 'autoload': False})
+    __bodId__ = 'ch.bav.projektierungszonen-eisenbahnanlagen.oereb'
+
+register_oereb(ProjektierungszonenEisenbahnanlagenOereb.__bodId__, ProjektierungszonenEisenbahnanlagenOereb)
+
+
+class BaulinienEisenbahnanlagenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'baulinien_eisenbahnanlagen_oereb'
+    __table_args__ = ({'schema': 'bav', 'autoload': False})
+    __bodId__ = 'ch.bav.baulinien-eisenbahnanlagen.oereb'
+
+register_oereb(BaulinienEisenbahnanlagenOereb.__bodId__, BaulinienEisenbahnanlagenOereb)
+
+
+# BAZL
+class ProjektierungszonenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'projektierungszonen_oereb'
+    __table_args__ = ({'schema': 'bazl', 'autoload': False})
+    __bodId__ = 'ch.bazl.projektierungszonen-flughafenanlagen.oereb'
+
+register_oereb(ProjektierungszonenOereb.__bodId__, ProjektierungszonenOereb)
+
+
+class SichereitszonenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'sichereitszonen_oereb'
+    __table_args__ = ({'schema': 'bazl', 'autoload': False})
+    __bodId__ = 'ch.bazl.sicherheitszonenplan.oereb'
+
+register_oereb(SichereitszonenOereb.__bodId__, SichereitszonenOereb)
+
+
+class KatasterBelastetenStandorteZivflplOereb(Base, OerebBase, Vector):
     __tablename__ = 'kataster_belasteter_standorte_zivflpl_oereb'
     __table_args__ = ({'schema': 'bazl', 'autoload': False})
     __bodId__ = 'ch.bazl.kataster-belasteter-standorte-zivilflugplaetze.oereb'
-    id = Column('stabil_id', Text, primary_key=True)
-    the_geom = Column(Geometry(geometry_type='GEOMETRY',
-                               dimension=2, srid=21781))
-    geomType = Column('geom_type', Text)
-    xmlData = Column('xml_data', Text)
-    bgdi_created = Column('bgdi_created', Text)
-    data_created = Column('data_created', Text)
 
-register_oereb('ch.bazl.kataster-belasteter-standorte-zivilflugplaetze.oereb', Kataster_belasteten_standorte_zivflpl_Oereb)
+register_oereb(KatasterBelastetenStandorteZivflplOereb.__bodId__, KatasterBelastetenStandorteZivflplOereb)
+
+
+class BaulinienFlughafenanlagenOereb(Base, OerebBase, Vector):
+    __tablename__ = 'baulinien_flughafenanlagen_oereb'
+    __table_args__ = ({'schema': 'bazl', 'autoload': False})
+    __bodId__ = 'ch.bazl.baulinien-flughafenanlagen.oereb'
+
+register_oereb(BaulinienFlughafenanlagenOereb.__bodId__, BaulinienFlughafenanlagenOereb)
