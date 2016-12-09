a7d7072acbc40ce0a069819ec0b6d0fc5e31c9a8
loicgasser <loicgasser4@gmail.com>
2016-12-09 15:31:19 +0100

    Add merged perimeter for swissbul2

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -2427,13 +2427,21 @@ class Dhm25MetadataPerimeter(Base, ShopStandardClass, Vector):
 register('ch.swisstopo.digitales-hoehenmodell_25.metadata', Dhm25MetadataPerimeter)
 
 
-class SwissBuildings3d2Perimeter(Base, ShopStandardClass, Vector):
+class SwissBuildings3d2Meta(Base, ShopStandardClass, Vector):
     __tablename__ = 'shop_perimeter_build3d2'
     __table_args__ = ({'autoload': False})
     __bodId__ = 'ch.swisstopo.swissbuildings3d_2.metadata'
     the_geom = Column(Geometry2D)
 
-register('ch.swisstopo.swissbuildings3d_2.metadata', SwissBuildings3d2Perimeter)
+
+class SwissBuildings3d2Perimeter(Base, ShopStandardClass, Vector):
+    __tablename__ = 'shop_perimeter_swissbuilding3d_2'
+    __table_args__ = ({'autoload': False})
+    __bodId__ = 'ch.swisstopo.swissbuildings3d_2.metadata'
+    the_geom = Column(Geometry2D)
+
+register('ch.swisstopo.swissbuildings3d_2.metadata', SwissBuildings3d2Meta)
+register_perimeter('ch.swisstopo.swissbuildings3d_2.metadata', SwissBuildings3d2Perimeter)
 
 
 class SwissBuildings3dPerimeter(Base, ShopStandardClass, Vector):
