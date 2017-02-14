15ac8165ed377b9222322ddc401cf533053e32e7
loicgasser <loicgasser4@gmail.com>
2017-02-14 17:14:30 +0100

    Update models scan

--- scripts/models_scan.py
+++ scripts/models_scan.py
@@ -10,9 +10,12 @@ The output format is a csv with | delimiter and it contains the following attrib
 Usage:
 
   .venv/bin/python scripts/models_scan.py > results.csv
+
+  .venv/bin/python scripts/models_scan.py fixme (to display all duplicated pkeys)
 """
 
 import re
+import sys
 from pyramid.paster import get_app
 
 
@@ -20,8 +23,8 @@ def getPGTables(layerId, engine, dbname, schema, tablename):
     sqlTemplate = 'EXPLAIN VERBOSE SELECT * FROM %s.%s'
     try:
         t = []
-        sqlQuery = sqlTemplate %(schema, tablename) 
-        records = engine.execute(sqlQuery) 
+        sqlQuery = sqlTemplate %(schema, tablename)
+        records = engine.execute(sqlQuery)
         for i in records:
             table = re.search('[Bitmap Heap|Index|Seq] Scan.* on ([^ ]+)', i[0])
             table = table.group(1) if table else None
@@ -38,31 +41,69 @@ def getTableReferences(dbmap, engines):
         tmp = []
         tmpColumns = []
         for model in models:
-            dbname = re.match(
-                '^.*\/{1}([a-z]*)',
-                model.metadata.__str__()).group(1)
+            matches = re.match(
+                '^.*\/{1}([a-z]*_solarkataster)|^.*\/{1}([a-z]*)',
+                model.metadata.__str__())
+            dbname = matches.group(1) if matches.group(1) else matches.group(2)
             engine = engines.get(dbname)
             schema = model.__table_args__['schema'] if 'schema' in model.__table_args__ else 'public'
             tablename = model.__tablename__
             columnNames = [k.name for k in model.__table__.columns]
             tmp.append(getPGTables(layerId, engine, dbname, schema, tablename))
             tmpColumns += columnNames
+        if len(models) > 1:
+            print layerId
         result = [item for sublist in tmp for item in sublist]
         result = sorted(list(set(result)))
         resultColumns = sorted(list(set(tmpColumns)))
-        yield layerId, result, resultColumns
+        yield layerId, result, resultColumns, len(models), dbname
 
 
 def main():
+    bodIdsWithMultipleModels = []
+    dbsWithMultipleModels = []
     # Load app to register the models
     app = get_app('production.ini')
+    from sqlalchemy.orm import scoped_session, sessionmaker
     from chsdi.models import bodmap, oerebmap, perimetermap, engines
     print 'layerId|data|columns'
     dbsmap = [bodmap, oerebmap, perimetermap]
     for dbmap in dbsmap:
-        for layerId, ref, refColumns in getTableReferences(dbmap, engines):
+        for layerId, ref, refColumns, nbTables, dbname in getTableReferences(dbmap, engines):
             print '%s|%s|%s' %(layerId, ','.join(ref), ','.join(refColumns))
+            if nbTables > 1:
+                bodIdsWithMultipleModels.append(layerId)
+                dbsWithMultipleModels.append(dbname)
 
+    if len(sys.argv) == 1:
+        sys.exit(0)
+
+    fixme = []
+    # Test if we don't have the same IDs
+    for b in range(0, len(bodIdsWithMultipleModels)):
+        bodId = bodIdsWithMultipleModels[b]
+        dbname = dbsWithMultipleModels[b]
+        if dbname == 'uvek_solarkataster':
+            continue
+        models = bodmap.get(bodId)
+        nbModels = len(models)
+        engine = engines.get(dbname)
+        session = scoped_session(sessionmaker(bind=engine))
+        for i in range(0, nbModels):
+            for j in range(i, nbModels):
+                if i != j:
+                    iColType = models[i].primary_key_column().type
+                    jColType = models[j].primary_key_column().type
+                    if type(iColType) == type(jColType):
+                        subQuery = session.query(models[i].primary_key_column())
+                        subQuery = subQuery.filter(
+                                models[i].primary_key_column() == models[j].primary_key_column())
+                        exists = session.query(subQuery.exists()).scalar()
+                        if exists:
+                            fixme.append(
+                                [dbname, bodId, models[i], models[j], models[i].__tablename__, models[j].__tablename__])
+    for m in fixme:
+        print m
 
 if __name__ == '__main__':
     main()
