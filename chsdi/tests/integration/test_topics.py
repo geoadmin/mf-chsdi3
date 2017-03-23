cb0d377fbccf4c51766d02f6dfc0aa8b4930a19e
loicgasser <loicgasser4@gmail.com>
2017-03-23 15:19:58 +0100

    Add groupId to topics

--- chsdi/tests/integration/test_topics.py
+++ chsdi/tests/integration/test_topics.py
@@ -9,6 +9,7 @@ class TestTopicsListingView(TestsBase):
         resp = self.testapp.get('/rest/services', status=200)
         self.assertTrue(resp.content_type == 'application/json')
         topics = resp.json['topics']
+        i = 1
         for topic in topics:
             self.assertTrue('id' in topic)
             self.assertTrue('backgroundLayers' in topic)
@@ -17,6 +18,10 @@ class TestTopicsListingView(TestsBase):
             self.assertTrue('defaultBackground' in topic)
             self.assertTrue('activatedLayers' in topic)
             self.assertTrue('plConfig' in topic)
+            self.assertTrue('groupId' in topic)
+            if i != len(topics):
+                self.assertGreaterEqual(topics[i]['groupId'], topics[i - 1]['groupId'])
+            i += 1
 
     def test_topics_with_cb(self):
         resp = self.testapp.get('/rest/services', params={'callback': 'cb_'}, status=200)
