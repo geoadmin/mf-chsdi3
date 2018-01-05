0aeef5b99c11eda6367b133031e48cdd79941436
Tobias Reber <toby@seidenfein.ch>
2018-01-05 14:13:25 +0100

    bfs volkszaehlung, betriebszaehlung and bfs.generalisierte-grenzen_agglomerationen_g{1|2}

--- chsdi/models/vector/edi.py
+++ chsdi/models/vector/edi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, Float, Unicode
 
 from chsdi.models import register, bases
 from chsdi.models.vector import Vector, Geometry2D
@@ -223,3 +223,77 @@ class FsmeImpfung(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register('ch.bag.zecken-fsme-impfung', FsmeImpfung)
+
+
+class VolkBetriebZaehlung:
+    __table_args__ = ({'schema': 'bfs', 'autoload': False})
+    __template__ = 'templates/htmlpopup/volk_betrieb_zaehlung.mako'
+    __label__ = 'reli'
+    __timeInstant__ = 'i_year'
+    id = Column('bgdi_id', Integer, primary_key=True)
+    i_year = Column('i_year', Unicode)
+    reli = Column('reli', Integer)
+    number = Column('i_number', Integer)
+    the_geom = Column(Geometry2D)
+
+
+class VolkszaehlungBevEinwohner(Base, VolkBetriebZaehlung, Vector):
+    __tablename__ = 'v_volkszaehlung_bevoelkerung_einwohner'
+    __bodId__ = 'ch.bfs.volkszaehlung-bevoelkerungsstatistik_einwohner'
+
+register(VolkszaehlungBevEinwohner.__bodId__, VolkszaehlungBevEinwohner)
+
+
+class VolkszaehlungGebaeudeGebaude(Base, VolkBetriebZaehlung, Vector):
+    __tablename__ = 'v_volkszaehlung_gebaeude_gebaeude'
+    __bodId__ = 'ch.bfs.volkszaehlung-gebaeudestatistik_gebaeude'
+
+register(VolkszaehlungGebaeudeGebaude.__bodId__, VolkszaehlungGebaeudeGebaude)
+
+
+class VolkszaehlungGebaudeWohnungen(Base, VolkBetriebZaehlung, Vector):
+    __tablename__ = 'v_volkszaehlung_gebaeude_wohnungen'
+    __bodId__ = 'ch.bfs.volkszaehlung-gebaeudestatistik_wohnungen'
+
+register(VolkszaehlungGebaudeWohnungen.__bodId__, VolkszaehlungGebaudeWohnungen)
+
+
+class VolkszaehlungBetriebArbeitstaetten(Base, VolkBetriebZaehlung, Vector):
+    __tablename__ = 'v_betriebsszaehlung_arbeitsstaetten'
+    __bodId__ = 'ch.bfs.betriebszaehlungen-arbeitsstaetten'
+
+register(VolkszaehlungBetriebArbeitstaetten.__bodId__, VolkszaehlungBetriebArbeitstaetten)
+
+
+class VolkszaehlungBetriebBeschaeftigte(Base, VolkBetriebZaehlung, Vector):
+    __tablename__ = 'v_betriebsszaehlung_beschaeft_voll'
+    __bodId__ = 'ch.bfs.betriebszaehlungen-beschaeftigte_vollzeitaequivalente'
+
+register(VolkszaehlungBetriebBeschaeftigte.__bodId__, VolkszaehlungBetriebBeschaeftigte)
+
+
+class GenGrenzenAgglo:
+    __table_args__ = ({'schema': 'bfs', 'autoload': False})
+    __template__ = 'templates/htmlpopup/gen_grenzen_agglo.mako'
+    __label__ = 'gmd_nr'
+    id = Column('bgdi_id', Integer, primary_key=True)
+    gmd_name = Column('gmd_name', Unicode)
+    gmd_nr = Column('gmd_nr', Integer)
+    acode = Column('acode', Integer)
+    aname = Column('aname', Unicode)
+    area_ha = Column('area_ha', Float)
+    the_geom = Column(Geometry2D)
+
+
+class GenGrenzenAggloG1(Base, GenGrenzenAgglo, Vector):
+    __tablename__ = 'gen_grenzen_agglo_g1'
+    __bodId__ = 'ch.bfs.generalisierte-grenzen_agglomerationen_g1'
+
+register(GenGrenzenAggloG1.__bodId__, GenGrenzenAggloG1)
+
+
+class GenGrenzenAggloG2(Base, GenGrenzenAgglo, Vector):
+    __tablename__ = 'gen_grenzen_agglo_g2'
+    __bodId__ = 'ch.bfs.generalisierte-grenzen_agglomerationen_g2'
+
+register(GenGrenzenAggloG2.__bodId__, GenGrenzenAggloG2)
