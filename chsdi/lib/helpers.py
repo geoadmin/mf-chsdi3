3501f24152f721f34bbf2477f342287813fe92c1
Marcel Clausen <marcel.clausen@swisstopo.ch>
2018-09-13 09:32:50 +0200

    extend regex to match linefeeds in ilike where statement

--- chsdi/lib/helpers.py
+++ chsdi/lib/helpers.py
@@ -177,7 +177,7 @@ def format_query(model, value):
     '''
     def escapeSQL(value):
         if u'ilike' in value:
-            match = re.search(r'([\w]+\s)(ilike|not ilike)(\s\'%)(.*)(%\')', value)
+            match = re.search(r'([\w]+\s)(ilike|not ilike)(\s\'%)([\s\S]*)(%\')', value)
             where = u''.join((
                 match.group(1).replace(u'\'', u'E\''),
                 match.group(2),
