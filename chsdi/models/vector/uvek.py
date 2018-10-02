0cdc6b335b90cc8e53ee8e7092c424f3074ac53b
alcapat <patrick.alcaraz.ch@gmail.com>
2018-10-02 17:09:06 +0200

    Fix tooltip display too much info

--- chsdi/models/vector/uvek.py
+++ chsdi/models/vector/uvek.py
@@ -153,7 +153,7 @@ register('ch.bazl.sicherheitszonenplan', SicherheitsZonenPlan)
 
 
 class IVSNat(Base, Vector):
-    __tablename__ = 'ivs_nat'
+    __tablename__ = 'ivs_nat_tt'
     __table_args__ = ({'schema': 'astra', 'autoload': False})
     __template__ = 'templates/htmlpopup/ivs_nat.mako'
     __bodId__ = 'ch.astra.ivs-nat'
@@ -174,7 +174,30 @@ class IVSNat(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register('ch.astra.ivs-nat', IVSNat)
-register('ch.astra.ivs-nat-verlaeufe', IVSNat)
+
+
+class IVSNatVerlaeufe(Base, Vector):
+    __tablename__ = 'ivs_nat_verlaeufe_tt'
+    __table_args__ = ({'schema': 'astra', 'autoload': False})
+    __template__ = 'templates/htmlpopup/ivs_nat.mako'
+    __bodId__ = 'ch.astra.ivs-nat-verlaeufe'
+    __queryable_attributes__ = ['ivs_slaname', 'ivs_nummer', 'ivs_signatur']
+    __label__ = 'id'
+    id = Column('nat_id', Integer, primary_key=True)
+    ivs_slaname = Column('ivs_slaname', Unicode)
+    ivs_nummer = Column('ivs_nummer', Unicode)
+    ivs_signatur = Column('ivs_signatur', Unicode)
+    ivs_signatur_fr = Column('ivs_signatur_fr', Unicode)
+    ivs_signatur_it = Column('ivs_signatur_it', Unicode)
+    ivs_signatur_de = Column('ivs_signatur_de', Unicode)
+    ivs_kanton = Column('ivs_kanton', Unicode)
+    ivs_sladatehist = Column('ivs_sladatehist', Integer)
+    ivs_sladatemorph = Column('ivs_sladatemorph', Integer)
+    ivs_slabedeutung = Column('ivs_slabedeutung', Integer)
+    ivs_sortsla = Column('ivs_sortsla', Unicode)
+    the_geom = Column(Geometry2D)
+
+register('ch.astra.ivs-nat-verlaeufe', IVSNatVerlaeufe)
 
 
 class IVSRegLoc(Base, Vector):
