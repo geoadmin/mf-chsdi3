c938c2bb0b9307365fac72a49ec05820252367d1
Marc Monnerat <procrastinatio@gmail.com>
2018-11-08 17:21:09 +0100

    return valid geojson (fix #3020)

--- chsdi/views/features.py
+++ chsdi/views/features.py
@@ -319,7 +319,10 @@ def _get_feature_service(request):
     features = []
     for feature, vector_model in _get_features(params):
         if hasattr(params, 'featureIds') and len(params.featureIds) > 1:
-            features.append(feature['feature'])
+            if hasattr(feature, 'feature'):
+                features.append(feature['feature'])
+            else:
+                features.append(feature)
         else:
             features.append(feature)
     if len(features) == 1:
