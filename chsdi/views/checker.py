56e9cfff899d6dbce574c5078a456f92b6cd9365
Marc Monnerat <procrastinatio@gmail.com>
2018-11-20 12:01:58 +0100

    checker checking DB and S3 (fix #3028)

--- chsdi/views/checker.py
+++ chsdi/views/checker.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from boto.exception import JSONResponseError
 from pyramid.view import view_config
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPInternalServerError
+from chsdi.views.files import FileView
 
 
-class Checker(object):
+class Checker(FileView):
 
     def __init__(self, request):
         self.request = request
+        self.dynamodb_table_name = 'geoadmin-file-'
+        self.bucket_key_name = 'geoadmin_file_storage_bucket66'
+        self.bucket_name = request.registry.settings['geoadmin_file_storage_bucket']
+        FileView.__init__(self, request)
 
     @view_config(route_name='checker')
     def home(self):
+        try:
+            self.dynamodb_fileshandler.table.count()
+        except (KeyError, JSONResponseError):
+            raise HTTPInternalServerError('Cannot access to DynamoDB backend {}'.format(self.dynamodb_fileshandler.table.table_name))
+
         return Response(body='OK', status_int=200)
 
     @view_config(route_name='checker_dev')
