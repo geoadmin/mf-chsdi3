fe6703742f3920ad4b9bd85238323c82133285ba
Tobias Reber <toby@seidenfein.ch>
2018-11-23 09:10:56 +0100

    ...

--- tests/integration/test_stationboard.py
+++ tests/integration/test_stationboard.py
@@ -12,13 +12,13 @@ class TestStationboard(TestsBase):
 
     def opentransapi_is_up(self):
         try:
-            self.testapp.get('/stationboard/stops/8501120', status=200)
+            resp = self.testapp.get('/stationboard/stops/8501120', status=200)
+            self.assertEqual(resp.status, 200)
         except (AppError, AssertionError):
             self.skipTest("OpenTrans API is down. Skipping iall  tests")
 
     def test_stationboard(self):
-        resp = self.testapp.get('/stationboard/stops/8501120', status=200)
-        self.assertEqual(resp.status, 200)
+        resp = self.testapp.get('/stationboard/stops/8501120', status=200) 
         self.assertEqual(resp.content_type, 'application/json')
 
     def test_stationboard_wrong_station(self):
