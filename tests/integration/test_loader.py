7cff487550ea87ca4e9d4c598aa4d229009b5c36
Marc Monnerat <procrastinatio@gmail.com>
2019-01-17 10:11:11 +0100

    improving tests

--- tests/integration/test_loader.py
+++ tests/integration/test_loader.py
@@ -4,42 +4,45 @@ from tests.integration import TestsBase
 
 
 class TestLoaderJs(TestsBase):
+    default_version = '4.4.2'
 
     def test_loaderjs(self):
         resp = self.testapp.get('/loader.js', status=200)
         self.assertTrue(resp.content_type, 'application/javascript')
-        resp.mustcontain('resources/api/3.6.0/ga.js')
-        resp.mustcontain('resources/api/3.6.0/ga.css')
-        resp.mustcontain('resources/api/3.6.0/EPSG21781.js')
-        resp.mustcontain('resources/api/3.6.0/EPSG2056.js')
-        resp.mustcontain('proj4.js')
+        resp.mustcontain('resources/api/{}/ga.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/ga.css'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG21781.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG2056.js'.format(self.default_version))
         resp.mustcontain('proj4.js')
 
     def test_loaderjs_lang(self):
-        resp = self.testapp.get('/loader.js', params={'lang': 'en'}, status=200)
+        resp = self.testapp.get('/loader.js', params={'lang': 'fr'}, status=200)
         self.assertTrue(resp.content_type, 'application/javascript')
-        resp.mustcontain('resources/api/3.6.0/ga.js')
-        resp.mustcontain('resources/api/3.6.0/ga.css')
-        resp.mustcontain('resources/api/3.6.0/EPSG21781.js')
-        resp.mustcontain('resources/api/3.6.0/EPSG2056.js')
+        resp.mustcontain('resources/api/{}/ga.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/ga.css'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG21781.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG2056.js'.format(self.default_version))
         resp.mustcontain('proj4.js')
+        # We use french, as word in English or German maybe found in layers'attributes.
+        self.assertTrue(resp.body.count('eau') >= 1)
 
     def test_loaderjs_debug(self):
         resp = self.testapp.get('/loader.js', params={'mode': 'debug'}, status=200)
         self.assertTrue(resp.content_type, 'application/javascript')
-        resp.mustcontain('resources/api/3.6.0/ga-debug.js')
-        resp.mustcontain('resources/api/3.6.0/ga.css')
-        resp.mustcontain('resources/api/3.6.0/EPSG21781.js')
-        resp.mustcontain('resources/api/3.6.0/EPSG2056.js')
+        resp.mustcontain('resources/api/{}/ga-debug.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/ga.css'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG21781.js'.format(self.default_version))
+        resp.mustcontain('resources/api/{}/EPSG2056.js'.format(self.default_version))
         resp.mustcontain('proj4.js')
 
     def test_loaderjs_version(self):
-        resp = self.testapp.get('/loader.js', params={'version': '3.18.2'}, status=200)
+        version = '3.18.2'
+        resp = self.testapp.get('/loader.js', params={'version': version}, status=200)
         self.assertTrue(resp.content_type, 'application/javascript')
-        resp.mustcontain('resources/api/3.18.2/ga.js')
-        resp.mustcontain('resources/api/3.18.2/ga.css')
-        resp.mustcontain('resources/api/3.18.2/EPSG21781.js')
-        resp.mustcontain('resources/api/3.18.2/EPSG2056.js')
+        resp.mustcontain('resources/api/{}/ga.js'.format(version))
+        resp.mustcontain('resources/api/{}/ga.css'.format(version))
+        resp.mustcontain('resources/api/{}/EPSG21781.js'.format(version))
+        resp.mustcontain('resources/api/{}/EPSG2056.js'.format(version))
         resp.mustcontain('proj4.js')
 
     def test_loaderjs_bad_version(self):
