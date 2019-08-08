b3d869c05e1abff2f94cd514840d78c53cd0b2fa
Marc Monnerat <procrastinatio@gmail.com>
2019-08-09 00:21:25 +0200

    signed number, boolean and more tests

--- tests/functional/test_parser.py
+++ tests/functional/test_parser.py
@@ -7,13 +7,15 @@ from chsdi.lib.parser import WhereParser
 class TestWhereParser(unittest.TestCase):
 
     def test_with_numbers(self):
-        sqls = ["toto = 1", "toto > 1", "toto >= 1.1"]
+        sqls = ["toto = 1", "toto > 1", "toto < -5.6",  "toto >= 1.1", "toto<=3", "toto=4", "toto < 1.0e6", "toto > -1.5677e3"]
+        expected = ["toto = 1", "toto > 1", "toto < -5.6",  "toto >= 1.1", "toto <= 3", "toto = 4", "toto < 1.0e6", "toto > -1.5677e3"]
         for i, sql in enumerate(sqls):
             w = WhereParser(sql)
-            self.assertEqual(sqls[i],  w.sql)
+            self.assertEqual(expected[i],  w.sql)
 
     def test_with_strings(self):
-        sqls = ["toto = 'raa'", "toto like 'ttu%'", "toto not ilike '%zaza%'"]
+        sqls = ["toto = 'raa'", "toto like 'ttu%'", "toto ilike '%urr%'", "toto not ilike 'Tata%'",
+                "toto not ilike '%zaza%'", u"name ilike '%Aare-Bern, Schönau%'", u'andere_stoffe is null']
         for i, sql in enumerate(sqls):
             w = WhereParser(sql)
             self.assertEqual(sqls[i],  w.sql)
@@ -24,10 +26,24 @@ class TestWhereParser(unittest.TestCase):
             w = WhereParser(sql)
             self.assertEqual(sqls[i],  w.sql)
 
+    def test_with_is_boolean(self):
+        sqls = ["toto is true", "toto is not true", "toto is false", "toto is not false",
+                "toto != false", "toto <> false", "toto = false", "toto = true", "toto=true", "toto=false"]
+        expected = ["toto is true", "toto is not true", "toto is false", "toto is not false",
+                "toto != false", "toto <> false", "toto = false", "toto = true", "toto = true", "toto = false"]
+        for i, sql in enumerate(sqls):
+            w = WhereParser(sql)
+            self.assertEqual(expected[i],  w.sql)
+
     def test_with_and(self):
         sql = "toto is null and toto is not null"
         w = WhereParser(sql)
         self.assertEqual("and",  w.operators[0])
+
+    def test_with_date(self):
+        sql = u"startofconstruction > '2014-12-01'"
+        w = WhereParser(sql)
+        self.assertEqual(sql,  w.sql)
 
     def test_with_or(self):
         sql = "toto like 'tutu%' or toto is not null"
