1e71399e78839871c99e996097536fee7565af67
Marc Monnerat <procrastinatio@gmail.com>
2019-08-08 12:17:07 +0200

    using a parsing library to parse where statements

---
+++ chsdi/lib/parser.py
@@ -0,0 +1,90 @@
+from lark import Lark, Transformer
+import lark
+
+where_gram = """
+
+?start: value
+
+?value: expressions
+     | expression
+
+
+expressions: expression (and_or expression)* [and_or]
+
+expression: WORD operators NUMBERS
+           | WORD operators_likes  ESCAPED_QUOTED_STRING
+
+operators: OPERATORS   -> ops
+operators_likes: OPERATORS | LIKES
+
+NUMBERS: /(\d+(\.\d+)?)/
+
+SINGLE_QUOTED_STRING: /'[^']*'/
+ESCAPED_QUOTED_STRING: /'(?:[^'\\\\]|\\\\.)*'/
+OPERATORS: "<="|">="|"<"|">"|"=="|">="|"<="|"<>"|"!="|"="
+LIKES: "ilike"|"not ilike"|"not like"|"like"
+OPERATORS_LIKES: OPERATORS | LIKES
+and_or: /and|or/i
+
+%import common.NUMBER
+%import common.WORD
+%import common.FLOAT
+%import common.INT
+%ignore /[\t \f]+/  // WS
+"""
+
+
+class WhereParser(object):
+
+    def __init__(self, text):
+        self.text = text
+        self.parser = Lark(where_gram, debug=True)
+        self.transformer = WhereTransformer()
+
+    @property
+    def sql(self):
+        return u" ".join(self._tokens())
+
+    @property
+    def tokens(self):
+        return filter(lambda x: x not in ('or', 'and'), self._tokens())
+
+    @property
+    def operators(self):
+        return filter(lambda x: x in ('or', 'and'), self._tokens())
+
+    def _tokens(self):
+        r = []
+        try:
+            tr = self.transformer.transform(self._tree())
+        except Exception:
+            return r
+        if isinstance(tr, lark.tree.Tree):
+            r = tr.children
+        else:
+            r.append(tr)
+
+        return r
+
+    def _tree(self):
+        tree = []
+        try:
+            tree = self.parser.parse(self.text)
+        except Exception as e:
+            print("Cannot parse '{}'".format(e))
+        return tree
+
+
+class WhereTransformer(Transformer):
+
+    def expression(self, args):
+        return u" ".join(map(unicode, args))
+
+    def and_or(self, s):
+        return unicode(s[0])
+
+    def ops(self, s):
+        return str(s[0])
+
+    def operators_likes(self, s):
+        return unicode(s[0])
