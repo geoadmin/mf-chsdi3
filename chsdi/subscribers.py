71527100e59ff97644c6d44d74a25e48197cb671
Marc Monnerat <procrastinatio@gmail.com>
2019-10-13 11:28:48 +0200

    lang should be a string not a binary

--- chsdi/subscribers.py
+++ chsdi/subscribers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import six
 from pyramid.i18n import get_localizer, TranslationStringFactory
 from chsdi.lib import helpers
 
@@ -19,7 +20,9 @@ def add_localizer(event):
     request._LOCALE_ = helpers.locale_negotiator(request)
     localizer = get_localizer(request)
     request.lang = 'rm' if localizer.locale_name == 'fi' else localizer.locale_name
-    request.lang = request.lang.encode('ascii', 'ignore')
+    # Python2/3
+    if not six.PY3:
+        request.lang = request.lang.encode('ascii', 'ignore')
     # The load balancer forwards requests as http, therefore we need to check X-Forwarded-Proto
     request.scheme = request.headers.get('X-Forwarded-Proto', request.scheme)
 
