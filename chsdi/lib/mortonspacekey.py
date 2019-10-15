0786bc87b819a0d8e62b3d4e3e37bddea519e75e
Marc Monnerat <procrastinatio@gmail.com>
2019-10-15 15:51:32 +0200

    still many mortonkey failing

--- chsdi/lib/mortonspacekey.py
+++ chsdi/lib/mortonspacekey.py
@@ -175,6 +175,9 @@ class QuadTree:
 
     def _getCommonKey(self, keys):
         res = ''
+        # Python2/3
+        if not isinstance(keys, list):
+            keys = list(keys)
         for i in range(self.levels + 1):
             before = len(res)
             if not reduce(lambda has, k: (has and len(k) > i), keys, True):
