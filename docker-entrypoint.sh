4b0595ecd448e7f9d28097007c50555e39216976
Marc Monnerat <procrastinatio@gmail.com>
2020-01-29 16:03:27 +0000

    port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,8 @@ export INSTALLDIR
 
 echo "INSTALLDIR=${INSTALLDIR}"
 
+source rc_prod && make production.ini development.ini
+
 envsubst < 25-mf-chsdi3.conf.in > /etc/apache2/sites-available/000-default.conf 
 
 envsubst < apache/application.wsgi.in > apache/application.wsgi
@@ -16,11 +18,8 @@ envsubst < apache/ports.conf.in > /etc/apache2/ports.conf
 
 # envsubst < default.conf.in > /etc/apache2/sites-enabled/000-default.conf
 
-
-
 envsubst < apache/wsgi.conf.in > apache/wsgi.conf
 
-source rc_prod && make production.ini development.ini
 
 
 # Always put this damn shit
