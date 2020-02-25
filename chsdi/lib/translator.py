f8075d0f86c0c52f6eb786f56132c872996d5b65
Martin Kuenzi <39826573+ltkum@users.noreply.github.com>
2020-02-25 16:27:20 +0100

    DBSTAGING rather than hard coded staging

--- chsdi/lib/translator.py
+++ chsdi/lib/translator.py
@@ -10,20 +10,40 @@ CACHE_TTL = 3600  # Cache duration in seconds --> 1 hour cache for translations
 CACHE_SIZE = 10000
 
 
+# This is a class with a singleton approach. We do not need to have twenty caches around the code
 class Translator:
 
     _translations = None
     _supported_languages = ['en', 'fr', 'de', 'rm', 'it']
 
+    """
+    
+    Database transation methods 
+    
+    
+    """
     @staticmethod
     def get_db_connection():
-        return psycopg2.connect(host=os.environ.get('DBHOST'), dbname='bod_master', user='www-data')
+        return psycopg2.connect(host=os.environ.get('DBHOST'), dbname=os.environ.get('DBSTAGING'), user='www-data')
 
     @staticmethod
     def select_all(cur):
         cur.execute('SELECT msg_id, de, fr, it, rm, en FROM translations ORDER BY msg_id;')
         return cur
 
+    @classmethod
+    def fill_translations(cls):
+        conn = cls.get_db_connection()
+        register_type(UNICODE)
+        conn.set_client_encoding('UTF8')
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur = cls.select_all(cur)
+        for row in cur:
+            for lang in cls._supported_languages:
+                cls._translations[lang][row['msg_id']] = row[lang]
+        conn.close()
+
+    # this is a simili singleton approach.
     @classmethod
     def get_translations(cls):
         if cls._translations is None:
@@ -36,26 +56,15 @@ class Translator:
     def translate(cls, msg_id, lang):
         translations = cls.get_translations()
         if lang not in cls._supported_languages:
+            # default language is german
             lang = 'de'
         if translations[lang].currsize == 0:
+            # we do not fill the cache unless it is empty
+            # todo : thread safe this
             cls.fill_translations()
         translated_value = translations[lang].get(msg_id, msg_id)
         return translated_value
 
     @classmethod
-    def fill_translations(cls):
-        conn = cls.get_db_connection()
-        register_type(UNICODE)
-        conn.set_client_encoding('UTF8')
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-        cur = cls.select_all(cur)
-        for row in cur:
-            for lang in cls._supported_languages:
-
-                cls._translations[lang][row['msg_id']] = row[lang]
-        conn.close()
-
-    @classmethod
     def empty_cache(cls):
-        for lang in cls._supported_languages:
-            cls._translations[lang] = TTLCache(maxsize=CACHE_SIZE, ttl=CACHE_TTL)
+        cls._translations = None
