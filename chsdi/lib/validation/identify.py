3afd7c4a162ff31b66bdf25a4ffa3970b2366ec6
Marc Mönnerat <procrastinatio@gmail.com>
2020-03-06 14:33:19 +0100

    fix for python3 (fix #3399) (#3398)

--- chsdi/lib/validation/identify.py
+++ chsdi/lib/validation/identify.py
@@ -187,7 +187,8 @@ class IdentifyServiceValidation(BaseFeaturesValidation):
             self._imageDisplay = list(map(float_raise_nan, value))
         except ValueError:
             raise HTTPBadRequest('Please provide numerical values for the parameter imageDisplay')
-        if self.tolerance > 0 and not all(i > 0 for i in self._imageDisplay):
+        # Python3 None > 0 --> TypeError
+        if self.tolerance is not None and self.tolerance > 0 and not all(i > 0 for i in self._imageDisplay):
             raise HTTPBadRequest('All values for parameter "imageDisplay" must be strictly positive if tolerance>0')
 
     @mapExtent.setter
