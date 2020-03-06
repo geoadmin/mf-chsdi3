386bb1fcc075ff1cb15b29afe28b3f0689dfda43
ltkum <kunzi.martin@gmail.com>
2020-03-06 15:18:11 +0100

    removing old translation tools

--- chsdi/subscribers.py
+++ chsdi/subscribers.py
@@ -8,11 +8,8 @@ from chsdi.lib import helpers
 def add_renderer_globals(event):
     request = event.get('request')
     if request:
-        event['_'] = request.translate
         event['localizer'] = request.localizer
         event['h'] = helpers
-
-tsf = TranslationStringFactory('chsdi')
 
 
 def add_localizer(event):
@@ -25,8 +22,4 @@ def add_localizer(event):
         request.lang = request.lang.encode('ascii', 'ignore')
     # The load balancer forwards requests as http, therefore we need to check X-Forwarded-Proto
     request.scheme = request.headers.get('X-Forwarded-Proto', request.scheme)
-
-    def auto_translate(string):
-        return localizer.translate(tsf(string))
     request.localizer = localizer
-    request.translate = auto_translate
