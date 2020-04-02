afdb653ec47c660de945b93c83deaac0a8c4f7e3
Marc Monnerat <procrastinatio@gmail.com>
2020-04-02 22:55:40 +0200

    disabling some tests

--- tests/integration/test_identify_service.py
+++ tests/integration/test_identify_service.py
@@ -7,7 +7,7 @@ import math
 from shapely.geometry import shape, Point, box
 
 
-S3_TESTS = os.environ.get('S3_TESTS', True)
+DO_S3_TESTS = int(os.environ.get('S3_TESTS', 1)) == 1
 
 accept_headers = {'Accept': 'application/json, text/plain, */*'}
 
@@ -352,7 +352,7 @@ class TestIdentifyService(TestsBase):
         resp = self.testapp.get('/rest/services/ech/MapServer/identify', params=params, headers=accept_headers, status=200)
         self.assertEqual(resp.content_type, 'application/json')
 
-    @skipUnless(S3_TESTS, "test requires AWS S3 access")
+    @skipUnless(DO_S3_TESTS, "test requires AWS S3 access")
     def test_identify_valid_on_grid(self):
         params = {'geometry': '555000,171125',
                   'geometryFormat': 'geojson',
@@ -374,7 +374,7 @@ class TestIdentifyService(TestsBase):
         self.assertGeojsonFeature(resp_2.json['results'][0], 2056)
         self.assertEqual(resp.json['results'][0]['id'], resp_2.json['results'][0]['id'])
 
-    @skipUnless(S3_TESTS, "test requires AWS S3 access")
+    @skipUnless(DO_S3_TESTS, "test requires AWS S3 access")
     def test_identify_valid_envelope_on_grid(self):
         params = {'geometry': '555000,171125,556000,172125',
                   'geometryFormat': 'geojson',
