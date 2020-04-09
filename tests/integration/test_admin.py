9bce10fc744c657949e85300681d03612ab1edc2
Marc Monnerat <procrastinatio@gmail.com>
2020-04-09 20:20:54 +0200

    default region for test

--- tests/integration/test_admin.py
+++ tests/integration/test_admin.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from unittest import skipUnless
-from tests.integration import TestsBase, dynamodb_tests
+from tests.integration import TestsBase, dynamodb_tests, s3_tests
 
 
 class TestAdminkml(TestsBase):
 
-    @skipUnless(dynamodb_tests, "No connection do AWS DynamoDB")
+    @skipUnless(dynamodb_tests and s3_tests, "No connection to AWS DynamoDB and/or AWS S3")
     def test_admin_kml(self):
         resp = self.testapp.get('/admin/kml', status=200)
         self.assertEqual(resp.content_type, 'text/html')
