e268ef345e0fec172633eb1aafb4cb8dae18ee98
Marc Mönnerat <procrastinatio@gmail.com>
2020-04-09 18:57:38 +0200

    Configurable DynamoDB tables (#3453)
    * make <shorturl> table configurable for tests, smae for  filestorage

--- chsdi/views/admin.py
+++ chsdi/views/admin.py
@@ -15,8 +15,9 @@ LIMIT = 50
 def admin_kml(context, request):
     settings = request.registry.settings
     bucket_name = settings.get('geoadmin_file_storage_bucket')
+    table_name = settings.get('geoadmin_file_storage_table')
     api_url = settings.get('api_url')
-    files = kml_load(api_url=api_url, bucket_name=bucket_name)
+    files = kml_load(api_url=api_url, bucket_name=bucket_name, table_name=table_name)
     kmls = {'files': files, 'count': len(files), 'bucket': api_url, 'host': settings.get('geoadminhost')}
 
     response = render_to_response(
@@ -26,10 +27,10 @@ def admin_kml(context, request):
     return response
 
 
-def kml_load(api_url='//api3.geo.admin.ch', bucket_name=None):
+def kml_load(api_url='//api3.geo.admin.ch', bucket_name=None, table_name=None):
     now = datetime.datetime.now()
     date = now.strftime('%Y-%m-%d')
-    table = get_dynamodb_table(table_name='geoadmin-file-storage')
+    table = get_dynamodb_table(table_name=table_name)
     fileids = []
     results = table.query(Limit=LIMIT * 4,
                           IndexName='bucketTimestampIndex',
