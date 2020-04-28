0d7cd5600311c19f493f41f0f5dd936e26a25d0a
Martin Kuenzi martin.kunzi@swisstopo.ch <39826573+ltkum@users.noreply.github.com>
2020-04-28 10:43:43 +0200

    WIP: trying to find why kmls are not written or not retrieved

--- chsdi/views/admin.py
+++ chsdi/views/admin.py
@@ -2,6 +2,7 @@
 
 import datetime
 import requests
+import logging
 from requests.exceptions import RequestException
 from pyramid.view import view_config
 from pyramid.renderers import render_to_response
@@ -17,6 +18,11 @@ def admin_kml(context, request):
     bucket_name = settings.get('geoadmin_file_storage_bucket')
     table_name = settings.get('geoadmin_file_storage_table')
     api_url = settings.get('api_url')
+    logging.debug("-----------------------------------------------------------------------")
+    logging.debug(bucket_name)
+    logging.debug(table_name)
+    logging.debug(api_url)
+    logging.debug("-----------------------------------------------------------------------")
     files = kml_load(api_url=api_url, bucket_name=bucket_name, table_name=table_name)
     kmls = {'files': files, 'count': len(files), 'bucket': api_url, 'host': settings.get('geoadminhost')}
 
@@ -32,17 +38,27 @@ def kml_load(api_url='//api3.geo.admin.ch', bucket_name=None, table_name=None):
     date = now.strftime('%Y-%m-%d')
     table = get_dynamodb_table(table_name=table_name)
     fileids = []
+    logging.debug("--------------------------------------------------------------------------------")
+    logging.debug(date)
+    logging.debug(bucket_name)
+    logging.debug(table_name)
+    logging.debug("--------------------------------------------------------------------------------")
+    logging.debug(table.load())
+    logging.debug("-------------------::::::::::::::::::::::::::::::::::::-------------------------")
     results = table.query(Limit=LIMIT * 4,
                           IndexName='bucketTimestampIndex',
-                          KeyConditionExpression=Key('bucket').eq(bucket_name) & Key('timestamp').begins_with(date),
+                          KeyConditionExpression=Key('bucket').eq(bucket_name) & Key('timestamp').begins_with('2'),
                           ScanIndexForward=False)
+    logging.debug("----------------------- ! ! ! ---------------------")
+    logging.debug(results['Items'])
+    logging.debug("----------------------- ! ! ! ---------------------")
     for f in results['Items']:
         try:
             resp = requests.head("http:" + api_url + "/files/" + f['fileId'], headers={'User-Agent': 'mf-geoadmin/python'})
             if int(resp.status_code) == 200:
                 fileids.append((f['fileId'], f['adminId'], f['timestamp']))
-        except RequestException:
-            pass
+        except RequestException as e:
+            logging.error(e)
         if len(fileids) >= LIMIT:
             return fileids
     return fileids
