d20ccd1809c9a852fd0bbe58fba6ea36acebb792
Simon1193 <53827463+Simon1193@users.noreply.github.com>
2020-05-19 11:01:42 +0200

    CHSDI ch.are.reisezeit (-miv/-oev) [UPDATE] (#3460)
    Co-authored-by: Marc Monnerat <procrastinatio@gmail.com>

--- chsdi/models/vector/are.py
+++ chsdi/models/vector/are.py
@@ -192,12 +192,34 @@ class ZweitwohnungsAnteil(Base, Vector):
 register('ch.are.wohnungsinventar-zweitwohnungsanteil', ZweitwohnungsAnteil)
 
 
+class Reisezeit:
+    __tablename__ = 'reisezeit'
+
+
 class Erreichbarkeit:
     __tablename__ = 'erreichbarkeit'
     __table_args__ = ({'schema': 'siedlung_landschaft', 'autoload': False, 'extend_existing': True})
     __label__ = 'id'
     id = Column('verkehrszone_id', Integer, primary_key=True)
     the_geom = Column(Geometry2D)
+
+
+class ReisezeitOev(Base, Reisezeit, Vector):
+    __bodId__ = 'ch.are.reisezeit-oev'
+    __template__ = 'templates/htmlpopup/reisezeit_oev.mako'
+    oev_reisezeit_z = Column('oev_reisezeit_z', Integer)
+    oev_no_z = Column('oev_no_z', Integer)
+
+register('ch.are.reisezeit-oev', ReisezeitOev)
+
+
+class ReisezeitMiv(Base, Reisezeit, Vector):
+    __bodId__ = 'ch.are.reisezeit-miv'
+    __template__ = 'templates/htmlpopup/reisezeit_miv.mako'
+    strasse_reisezeit_z = Column('strasse_reisezeit_z', Integer)
+    strasse_no_z = Column('strasse_no_z', Integer)
+
+register('ch.are.reisezeit-miv', ReisezeitMiv)
 
 
 class ErreichbarkeitOev(Base, Erreichbarkeit, Vector):
@@ -214,6 +236,7 @@ class ErreichbarkeitMiv(Base, Erreichbarkeit, Vector):
     strasse_erreichb_ewap = Column('strasse_erreichb_ewap', Integer)
 
 register('ch.are.erreichbarkeit-miv', ErreichbarkeitMiv)
+
 
 class BelastungPersonenverkehrStrasse(Base, Vector):
     __tablename__ = 'belastung_personenverkehr'
@@ -263,4 +286,3 @@ class BelastungPersonenverkehrBahn(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register(BelastungPersonenverkehrBahn.__bodId__, BelastungPersonenverkehrBahn)
-
