768b95a78b6190919d440ec32669f46fcbe53238
Marcel Clausen <marcel.clausen@swisstopo.ch>
2020-05-25 13:11:13 +0200

    change feature id

--- chsdi/models/vector/are.py
+++ chsdi/models/vector/are.py
@@ -194,13 +194,13 @@ register('ch.are.wohnungsinventar-zweitwohnungsanteil', ZweitwohnungsAnteil)
 
 class ReisezeitAgglomerationen:
     __tablename__ = 'reisezeit_agglomerationen'
-    id = Column('bgdi_id', Integer, primary_key=True)
+    id = Column('verkehrszone_id', Integer, primary_key=True)
     the_geom = Column(Geometry2D)
 
 
 class Reisezeit:
     __tablename__ = 'reisezeit'
-    id = Column('bgdi_id', Integer, primary_key=True)
+    id = Column('verkehrszone_id', Integer, primary_key=True)
     the_geom = Column(Geometry2D)
 
 
