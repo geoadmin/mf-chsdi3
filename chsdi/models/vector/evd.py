ea906a64bc155abee18e6c6b13c041e35e1cfdb5
Marc Fasel <mfasel@ip-10-220-4-246.eu-west-1.compute.internal>
2020-08-27 08:51:54 +0200

    Updated model

--- chsdi/models/vector/evd.py
+++ chsdi/models/vector/evd.py
@@ -266,3 +266,19 @@ class UrsprungsbezeichnungenKaese(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register('ch.blw.ursprungsbezeichnungen-kaese', UrsprungsbezeichnungenKaese)
+
+
+class UrsprungsbezeichnungenKonditoreiwaren(Base, Vector):
+    __tablename__ = 'ursprungsbezeichnungen_konditoreiwaren'
+    __table_args__ = ({'schema': 'blw', 'autoload': False})
+    __bodId__ = 'ch.blw.ursprungsbezeichnungen-konditoreiwaren'
+    __template__ = 'templates/htmlpopup/ursprungsbezeichnungen.mako'
+    __label__ = 'objektcode'
+    id = Column('bgdi_id', Integer, primary_key=True)
+    objektcode = Column('objektcode', Integer)
+    objekt_d = Column('objekt_d', Unicode)
+    objekt_f = Column('objekt_f', Unicode)
+    objekt_i = Column('objekt_i', Unicode)
+    the_geom = Column(Geometry2D)
+
+register('ch.blw.ursprungsbezeichnungen-konditoreiwaren', UrsprungsbezeichnungenKonditoreiwaren)
