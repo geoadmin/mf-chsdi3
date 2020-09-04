f39cefd1ffddcd46ee2e816226999cdb121e75da
Balthasar Teuscher <balthasar.teuscher@swisstopo.ch>
2020-09-04 13:02:00 +0200

    Add model

---
+++ chsdi/models/vector/oereb/vbs.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Unicode
+
+from chsdi.models import register_oereb, bases
+from chsdi.models.vector import Vector, Geometry2D
+
+Base = bases['vbs']
+
+
+class OerebBase:
+    id = Column('stabil_id', Unicode, primary_key=True)
+    the_geom = Column(Geometry2D)
+    geomType = Column('geom_type', Unicode)
+    xmlData = Column('xml_data', Unicode)
+    bgdi_created = Column('bgdi_created', Unicode)
+    data_created = Column('data_created', Unicode)
+
+
+# MILITAER
+class KatasterBelastetenStandorteMilitaerOereb(Base, OerebBase, Vector):
+    __tablename__ = 'kataster_belasteter_standorte_militaer_oereb'
+    __table_args__ = ({'schema': 'militaer', 'autoload': False})
+    __bodId__ = 'ch.bav.kataster-belasteter-standorte-militaer.oereb'
+
+register_oereb(KatasterBelastetenStandorteMilitaerOereb.__bodId__, KatasterBelastetenStandorteMilitaerOereb)
