d960b25876030a5ea7fae1d174e73fb326aa2542
Marcel Cläusen <5286659+ltclm@users.noreply.github.com>
2020-09-14 10:37:16 +0200

    adapt gwr model and mako to new data structure delivered via sedex (#3531)
    * initial import new sedex based model and mako for gebaeuderegister
    
    * update legends
    
    * remove deprecated sitemap service
    
    * adapt tests to new gwr model
    
    * change type
    
    * ewid can be None
    
    * add link to object
    
    * feedback mario
    
    * show language dependent street name
    
    * map langauge
    
    * translate attribute values (domain values and boolean value)
    
    * return untranslated value if tranlsation is missing
    
    * format string into unicode
    
    * change order in legend
    
    * use edi database for gwr addresses

--- chsdi/__init__.py
+++ chsdi/__init__.py
@@ -92,7 +92,6 @@ def main(global_config, **settings):
     config.add_route('wmtscapabilities', '/rest/services/{map}/1.0.0/WMTSCapabilities.xml')
     config.add_route('feedback', '/feedback')
     config.add_route('qrcodegenerator', '/qrcodegenerator')
-    config.add_route('sitemap', '/sitemap')
     config.add_route('luftbilder', '/luftbilder/viewer.html')
     config.add_route('historicalmaps', '/historicalmaps/viewer.html')
     config.add_route('checker', '/checker')
