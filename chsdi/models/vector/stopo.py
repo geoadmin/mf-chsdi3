2c8f23f26f2374390aca2e90551e5ce477ffe504
Marcel Clausen <marcel.clausen@swisstopo.ch>
2020-11-05 16:22:26 +0100

    fix wrong model

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -2410,7 +2410,6 @@ class Oerebkataster:
     telefon = Column('telefon', Unicode)
     email = Column('email', Unicode)
     url_oereb = Column('url_oereb', Unicode)
-    pdf_url = Column('pdf_url', Unicode)
     the_geom = Column(Geometry2D)
 
 
@@ -2419,6 +2418,7 @@ class OerebkatasterZoom1(Base, Oerebkataster, Vector):
     oereb_webservice = Column('oereb_webservice', Unicode)
     bgdi_status = Column('bgdi_status', Integer)
     egris_egrid = Column('egris_egrid', Integer)
+    pdf_url = Column('pdf_url', Unicode)
     number = Column('number_', Integer)
     __minscale__ = 1
     __maxscale__ = 50000
