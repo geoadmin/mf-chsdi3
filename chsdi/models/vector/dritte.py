2b62efcb1be407c85bc6fa9796ee1430a128bf76
faselm <marc.fasel@camptocamp.com>
2021-05-03 09:20:58 +0200

    Remove mcpfe

--- chsdi/models/vector/dritte.py
+++ chsdi/models/vector/dritte.py
@@ -71,7 +71,6 @@ class PronaturaWaldreservate(Base, Vector):
     gisflaeche = Column('obj_gisflaeche', Float)
     gesflaeche = Column('obj_gesflaeche', Float)
     gisteilobjekt = Column('obj_gisteilobjekt', Float)
-    mcpfe = Column('mcpfe', Unicode)
     mcpfe_class_de = Column('mcpfe_class_de', Unicode)
     mcpfe_class_fr = Column('mcpfe_class_fr', Unicode)
     mcpfe_class_it = Column('mcpfe_class_it', Unicode)
