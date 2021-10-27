e4c874d55ee19e4e84c6c1517fd4228be105e90a
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2021-10-27 12:09:25 +0200

    deactivating authorization header checking

--- chsdi/lib/decorators/__init__.py
+++ chsdi/lib/decorators/__init__.py
@@ -24,14 +24,16 @@ def requires_authorization():
     def wrapper(f):
         @wraps(f)
         def wrapped(self, *args, **kwargs):
-            if hasattr(self, 'request'):
-                request = self.request
-            else:
-                request = self
-            if request.headers.get('X-SearchServer-Authorized', '').lower() != 'true':
-                raise exc.HTTPForbidden(detail='This service requires an authorization')
-            else:
-                return f(self, *args, **kwargs)
+            # deativated by boc due to unplanned changes in varnish
+            # if hasattr(self, 'request'):
+            #    request = self.request
+            # else:
+            #    request = self
+            # if request.headers.get('X-SearchServer-Authorized', '').lower() != 'true':
+            #    raise exc.HTTPForbidden(detail='This service requires an authorization')
+            # else:
+            #    return f(self, *args, **kwargs)
+            return f(self, *args, **kwargs)
         return wrapped
     return wrapper
 
