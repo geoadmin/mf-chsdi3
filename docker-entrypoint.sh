45917b1d0f38daf94b88781b1d4a8e67656aaf72
Marc Monnerat <procrastinatio@gmail.com>
2022-05-20 14:57:26 +0200

    simplifying (only production.ini)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,7 +16,8 @@ echo "INSTALLDIR=${INSTALLDIR}"
 
 pg_isready -d stopo_prod -h ${DBHOST} -p ${DBPORT} -U ${PGUSER}
 
-source rc_${DEPLOY_TARGET} && make production.ini development.ini
+#source rc_${DEPLOY_TARGET} && make production.ini development.ini
+envsubst < production-py3.ini.in > production.ini
 
 envsubst < 25-mf-chsdi3.conf.in > /etc/apache2/sites-available/000-default.conf 
 
