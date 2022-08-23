7ff3c400971adbbe6954c17520f1849d004a3bd0
ltkum <kunzi.martin@gmail.com>
2022-08-23 10:39:05 +0200

    https://jira.swisstopo.ch/browse/BGDIINF_SB-2506
    Adapting The Dockerfile and Makefile to use pipenv instead of venv.
    
    removing old requirement file for python 3
    
    moving the pipenv cleaning part to another target, as pipenv --rm crash if pipenv has not been set within codebuild
    
    Reverting some changes, as the goal is to have minimal impact on the
    project and how it works.
    
    adding the line which makes the setup for the chsdi project
    
    timestamps are not used yet
    
    We can't remove requirements-py3.txt yet.
    Added dev packages category in Pipfile
    Correction to the makefile to also install dev packages and use the
    setup script when serving
    
    a comment to remember why the file is still here
    
    removing some verbosity during installation
    
    The setup.py script called by pipenv install -e no longer require the
    'requirements-py3.txt' file. Instead, it generates a requirements file
    on the fly (if it doesn't exist already) to feed the setup function.
    
    This new file is cleaned up by the makefile target "clean" and is
    part of the gitignore.
    
    removing print statement
    
    adding chsdi distribution in the buildspec to allow tests to run
    
    installation in build
    
    avoid git dirty error with setup script
    
    generating requirement file from pipfile
    
    removing requirements file from ignore file, as they are no longer generated
    
    using both standard and dev dependencies in setup.py
    
    seems like there is a problem between my local git history and the remote, trying to correct this
    
    using a non deprecated method for requirements creation
    
    removing unused package from imports
    
    removing the dev packages (which don't exist) to see if this solves the memory issue
    
    removing requirements from setup, as the system should already be installed at this point
    
    using env file to store env variables meant to stop pipenv verbosity
    
    By adding this package, the Pipfile (and lock) no longer gets modified by the
    installation of the chsdi package.
    
    removing useless install
    
    removed useless install command
    
    we moved the editable instruction within the pipfile to avoid changing pipfiles during the CI (and during development). As a consequence, we can no longer install packages before copying the files within the container.

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 import os
 import sys
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
-
-REQUIREMENTS_FILE = 'requirements-py3.txt'
-if sys.version_info < (3, 0, 0):
-    REQUIREMENTS_FILE = 'requirements.txt'
-
-requires = open(os.path.join(here, REQUIREMENTS_FILE)).read().split('\n')
 
 setup(name='chsdi',
       version='3.2.0',
@@ -37,9 +30,7 @@ setup(name='chsdi',
       package_data = {'chsdi': ['locale/*/LC_MESSAGES/*.mo']},
       include_package_data=True,
       zip_safe=False,
-      install_requires=requires,
       python_requires='>2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-      tests_require=requires,
       test_suite="chsdi",
       entry_points="""\
       [paste.app_factory]
