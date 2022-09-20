81efd97974a913c44f4f57a855cf8c9a6b6ed644
Brice Schaffner <brice.schaffner@swisstopo.ch>
2022-09-20 09:12:31 +0200

    BGDIINF_SB-2486: Added support for dynamic gunicorn workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,13 @@
 import os
 
+WSGI_WORKERS = int(os.getenv("WSGI_WORKERS", 0))
+if WSGI_WORKERS <= 0:
+    from multiprocessing import cpu_count
+    WSGI_WORKERS = (cpu_count() * 2) + 1
+
 bind = f"0.0.0.0:{os.getenv('HTTP_PORT', 8009)}"
 worker_class = "gevent"
-workers = os.getenv("WSGI_WORKERS", 0)
+workers = WSGI_WORKERS
 timeout = os.getenv("WSGI_TIMEOUT}", 30)
 forwarded_allow_ips = "*"
 secure_scheme_headers = {
