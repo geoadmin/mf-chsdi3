8caf2f1db41343989ae17bf507008cab46881511
faselm <marc.fasel@camptocamp.com>
2023-04-14 12:00:17 +0200

    BGDIDIC-2583: update models

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -3051,49 +3051,38 @@ class FixpunkteHfp2(Base, Vector):
 register(FixpunkteHfp2.__bodId__, FixpunkteHfp2)
 
 
-class SwisstneBasePoint(Base, Vector):
-    __tablename__ = 'swisstne_base_point'
+class SwisstneBase:
     __table_args__ = ({'schema': 'tlm', 'autoload': False})
-    __template__ = 'templates/htmlpopup/swisstne_base_point.mako'
     __bodId__ = 'ch.swisstne-base'
     __label__ = 'uuid'
     id = Column('bgdi_id', Unicode, primary_key=True)
     uuid = Column('uuid', Unicode)
     the_geom = Column(Geometry2D)
 
-register(SwisstneBasePoint.__bodId__, SwisstneBasePoint)
+
+class SwisstneBasePoint(Base, SwisstneBase, Vector):
+    __tablename__ = 'swisstne_base_point'
+    __template__ = 'templates/htmlpopup/swisstne_base_point.mako'
 
 
-class SwisstneBaseLine(Base, Vector):
+class SwisstneBaseLine(Base, SwisstneBase, Vector):
     __tablename__ = 'swisstne_base_line'
-    __table_args__ = ({'schema': 'tlm', 'autoload': False})
     __template__ = 'templates/htmlpopup/swisstne_base_line.mako'
-    __bodId__ = 'ch.swisstne-base'
-    __label__ = 'uuid'
-    id = Column('bgdi_id', Unicode, primary_key=True)
-    uuid = Column('uuid', Unicode)
     rail = Column('rail', Integer)
     road = Column('road', Integer)
     cableway = Column('cableway', Integer)
     water = Column('water', Integer)
-    the_geom = Column(Geometry2D)
-
-register(SwisstneBaseLine.__bodId__, SwisstneBaseLine)
 
 
-class SwisstneBasePoly(Base, Vector):
+class SwisstneBasePoly(Base, SwisstneBase, Vector):
     __tablename__ = 'swisstne_base_poly'
-    __table_args__ = ({'schema': 'tlm', 'autoload': False})
     __template__ = 'templates/htmlpopup/swisstne_base_poly.mako'
-    __bodId__ = 'ch.swisstne-base'
-    __label__ = 'uuid'
-    id = Column('bgdi_id', Unicode, primary_key=True)
-    uuid = Column('uuid', Unicode)
     type_poly = Column('type', Unicode)
     type_de = Column('type_de', Unicode)
     type_fr = Column('type_fr', Unicode)
     type_it = Column('type_it', Unicode)
     type_en = Column('type_en', Unicode)
-    the_geom = Column(Geometry2D)
 
-register(SwisstneBasePoly.__bodId__, SwisstneBasePoly)
+register(SwisstneBase.__bodId__, SwisstneBasePoint)
+register(SwisstneBase.__bodId__, SwisstneBaseLine)
+register(SwisstneBase.__bodId__, SwisstneBasePoly)
