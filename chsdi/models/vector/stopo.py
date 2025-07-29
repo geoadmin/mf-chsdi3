e5b2b072374dd3ca2f6ec403a52b236c4594f863
Marc Fasel marc.fasel@swisstopo.ch <marc.fasel@swisstopo.ch>
2025-07-29 14:49:10 +0200

    add missing model fields

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -1429,19 +1429,32 @@ register(GeologieRohstoffeGipsAbbauVerarbeitung.__bodId__, GeologieRohstoffeGips
 
 
 class GeologieRohstoffeIndustrieminerale(Base, Vector):
-    __tablename__ = 'rohstoffe_industriemin'
+    __tablename__ = 'rohstoffe_industrieminerale'
     __table_args__ = ({'schema': 'geol', 'autoload': False})
     __template__ = 'templates/htmlpopup/rohstoffe_industrieminerale.mako'
     __bodId__ = 'ch.swisstopo.geologie-rohstoffe-industrieminerale'
     __label__ = 'obname'
     id = Column('obid', Integer, primary_key=True)
     obname = Column('obname', Unicode)
-    obnamealt = Column('obnamealt', Unicode)
-    imkinds = Column('imkinds', Unicode)
-    edrskinds = Column('edrskinds', Unicode)
-    emkinds = Column('emkinds', Unicode)
-    stkind = Column('stkind', Unicode)
+    rskind_de = Column('rskind_de', Unicode)
+    rskind_fr = Column('rskind_fr', Unicode)
+    rskind_it = Column('rskind_it', Unicode)
+    rskind_en = Column('rskind_en', Unicode)
+    is_crm_eu_de = Column('is_crm_eu_de', Unicode)
+    is_crm_eu_fr = Column('is_crm_eu_fr', Unicode)
+    is_crm_eu_it = Column('is_crm_eu_it', Unicode)
+    is_crm_eu_en = Column('is_crm_eu_en', Unicode)
+    crm_etransition_de = Column('crm_etransition_de', Unicode)
+    crm_etransition_fr = Column('crm_etransition_fr', Unicode)
+    crm_etransition_it = Column('crm_etransition_it', Unicode)
+    crm_etransition_en = Column('crm_etransition_en', Unicode)
+    crm_factsheet_link = Column('crm_factsheet_link', Unicode)
+    mat_min_link_de = Column('mat_min_link_de', Unicode)
+    mat_min_link_fr = Column('mat_min_link_fr', Unicode)
+    mat_min_link_it = Column('mat_min_link_it', Unicode)
+    mat_min_link_en = Column('mat_min_link_en', Unicode)
     purl = Column('purl', Unicode)
+    swissgeol_link = Column('swissgeol_link', Unicode)
     the_geom = Column(Geometry2D)
 
 register(GeologieRohstoffeIndustrieminerale.__bodId__, GeologieRohstoffeIndustrieminerale)
@@ -3359,3 +3372,42 @@ class HoheitsgrenzeLandesvermessung(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register(HoheitsgrenzeLandesvermessung.__bodId__, HoheitsgrenzeLandesvermessung)
+
+
+class GeologieGeomolHoeheTopOmmData:
+    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __bodId__ = 'ch.swisstopo.geologie-geomol_hoehe_top_omm_data'
+    id = Column('bgdi_id', Integer, primary_key=True)
+    the_geom = Column(Geometry2D)
+
+
+class GeologieGeomolHoeheTopOmmDataWells(Base, GeologieGeomolHoeheTopOmmData, Vector):
+    __tablename__ = 'geomol_hoehe_top_omm_data_wells'
+    __template__ = 'templates/htmlpopup/geomol_hoehe_top_omm_data_wells.mako'
+    __label__ = 'well_name'
+    well_name = Column('well_name', Unicode)
+    link_viewer = Column('link_viewer', Unicode)
+    link_assets = Column('link_assets', Unicode)
+    link_boreholes = Column('link_boreholes', Unicode)
+
+register(GeologieGeomolHoeheTopOmmData.__bodId__, GeologieGeomolHoeheTopOmmDataWells)
+
+
+class GeologieGeomolHoeheTopOmmDataSeismicAll(Base, GeologieGeomolHoeheTopOmmData, Vector):
+    __tablename__ = 'geomol_hoehe_top_omm_data_seismic_all'
+    __template__ = 'templates/htmlpopup/geomol_hoehe_top_omm_data_seismic_all.mako'
+    __label__ = 'line_name'
+    line_name = Column('line_name', Unicode)
+    link_viewer = Column('link_viewer', Unicode)
+
+register(GeologieGeomolHoeheTopOmmData.__bodId__, GeologieGeomolHoeheTopOmmDataSeismicAll)
+
+
+class GeologieGeomolHoeheTopOmmDataProfileAll(Base, GeologieGeomolHoeheTopOmmData, Vector):
+    __tablename__ = 'geomol_hoehe_top_omm_data_profile_all'
+    __template__ = 'templates/htmlpopup/geomol_hoehe_top_omm_data_profile_all.mako'
+    __label__ = 'profil_name'
+    profil_name = Column('profil_name', Unicode)
+    link_viewer = Column('link_viewer', Unicode)
+
+register(GeologieGeomolHoeheTopOmmData.__bodId__, GeologieGeomolHoeheTopOmmDataProfileAll)
