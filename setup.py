34dfb4c846e1ea3b550567eaa7a198b45668c8ca
Marc Sommerhalder <marc.sommerhalder@swisstopo.ch>
2025-09-02 08:39:58 +0200

    PB-1892: Remove deprecated test_suite argument

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(name='chsdi',
       include_package_data=True,
       zip_safe=False,
       python_requires='>2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-      test_suite="chsdi",
       entry_points="""\
       [paste.app_factory]
       main = chsdi:main
