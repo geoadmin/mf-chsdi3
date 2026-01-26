c761101e9da5452fc3aadc9234df5bb68909708c
Khôi Tran <khoi.tran@swisstopo.ch>
2026-01-26 11:02:07 +0100

    PB-2179: re-use strtobool from helpers

--- chsdi/lib/helpers.py
+++ chsdi/lib/helpers.py
@@ -477,14 +477,6 @@ def get_payload(obj, trim=128):
     return '<not a text format>'
 
 
-def strtobool(value):
-    """Convert a string representation of truth to True or False.
-
-    Replaces deprecated https://github.com/python/cpython/blob/3.10/Lib/distutils/util.py#L308
-    """
-    value = value.lower()
-    if value in ('y', 'yes', 't', 'true', 'on', '1'):
-        return True
-    if value in ('n', 'no', 'f', 'false', 'off', '0'):
-        return False
-    raise ValueError(f"invalid truth value {value!r}")
+def strtobool(value: str) -> bool:
+    from chsdi.utils.strtobool import strtobool as s
+    return s(value)
--- chsdi/utils/otel.py
+++ chsdi/utils/otel.py
@@ -10,19 +10,7 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-
-def strtobool(value: str) -> bool:
-    """Convert a string representation of truth to true (1) or false (0).
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
-    """
-    value = value.lower()
-    if value in ('y', 'yes', 't', 'true', 'on', '1'):
-        return True
-    if value in ('n', 'no', 'f', 'false', 'off', '0'):
-        return False
-    raise ValueError(f"invalid truth value \'{value}\'")
+from chsdi.utils.strtobool import strtobool
 
 
 def instrument_pyramid(config):
