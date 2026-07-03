ab018204c1df25b5c0b481db5d9e6b0c79a88371
Tobias Reber <tobias.reber@swisstopo.ch>
2026-07-03 09:38:22 +0200

    BGDIDIC-1750: Fix the identifies for swissgeocover2d

--- chsdi/models/vector/stopo.py
+++ chsdi/models/vector/stopo.py
@@ -2432,8 +2432,7 @@ register(SwissGeocover2dBedrock.__bodId__, SwissGeocover2dBedrock)
 
 
 class SwissGeocover2dUnconsolidated(Base, Vector, SwissGeocover2d):
-    __tablename__ = 'swissgeocover2d_unconsolidated'
-    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'swissgeocover2d_unco_deposits'
     __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_unconsolidated'
     __template__ = 'templates/htmlpopup/swissgeocover2d_unconsolidated.mako'
     __label__ = 'label'
@@ -2460,8 +2459,7 @@ register(SwissGeocover2dUnconsolidated.__bodId__, SwissGeocover2dUnconsolidated)
 
 
 class SwissGeocover2dSurfaces(Base, Vector, SwissGeocover2dExtended):
-    __tablename__ = 'swissgeocover2d_surfaces'  # TODO: rename to geocover_surfaces
-    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'swissgeocover2d_surfaces'
     __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_surfaces'
     __template__ = 'templates/htmlpopup/swissgeocover2d_surfaces.mako'
     __label__ = 'kind_de'
@@ -2473,8 +2471,7 @@ register(SwissGeocover2dSurfaces.__bodId__, SwissGeocover2dSurfaces)
 
 
 class SwissGeocover2dLines(Base, Vector, SwissGeocover2dExtended):
-    __tablename__ = 'swissgeocover2d_lines'  # TODO: rename to geocover_linear_objects
-    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'swissgeocover2d_linear_objects'
     __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_lines'
     __template__ = 'templates/htmlpopup/swissgeocover2d_lines.mako'
     __label__ = 'kind_de'
@@ -2486,10 +2483,10 @@ register(SwissGeocover2dLines.__bodId__, SwissGeocover2dLines)
 
 
 class SwissGeocover2dTectoLines(Base, Vector, SwissGeocover2dExtended):
-    # Shares the geocover_ltmom_linear_objects PostGIS table
-    __tablename__ = 'swissgeocover2d_tecto-lines'  # TODO: rename to geocover_linear_objects
+    # Shares the swissgeocover_linear_objects PostGIS table
+    __tablename__ = 'swissgeocover2d_linear_objects'
     __table_args__ = ({'schema': 'geol', 'autoload': False, 'extend_existing': True})
-    __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_tecto-lines'
+    __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_tecto_lines'
     __template__ = 'templates/htmlpopup/swissgeocover2d_tecto_lines.mako'
     __label__ = 'kind_de'
     kind_de = Column('kind_de', Unicode)
@@ -2500,8 +2497,7 @@ register(SwissGeocover2dTectoLines.__bodId__, SwissGeocover2dTectoLines)
 
 
 class SwissGeocover2dPoints(Base, Vector, SwissGeocover2dExtended):
-    __tablename__ = 'swissgeocover2d_points'
-    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'swissgeocover2d_point_objects'
     __bodId__ = 'ch.swisstopo.geologie-swissgeocover2d_points'
     __template__ = 'templates/htmlpopup/swissgeocover2d_points.mako'
     __label__ = 'kind_de'
@@ -2528,8 +2524,7 @@ register(SwissGeocover2dPoints.__bodId__, SwissGeocover2dPoints)
 
 
 class SwissGeocover2dFossils(Base, Vector, SwissGeocover2dExtended):
-    __tablename__ = 'swissgeocover2d_fossils'  # TODO: rename to geocover_fossils
-    __table_args__ = ({'schema': 'geol', 'autoload': False})
+    __tablename__ = 'swissgeocover2d_fossils'
     __bodId__ = SwissGeocover2dPoints.__bodId__
     __template__ = 'templates/htmlpopup/swissgeocover2d_fossils.mako'
     __label__ = 'kind_de'
