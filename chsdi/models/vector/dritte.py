34fc0eb4e6e1bae67b2dd52be53b650b42718fea
Julian Hafner <julian.hafner@camptocamp.com>
2026-07-30 09:25:20 +0200

    BGDIDIC-3053 Add tooltips for treibhausgasemissionen

--- chsdi/models/vector/dritte.py
+++ chsdi/models/vector/dritte.py
@@ -234,3 +234,20 @@ class AgroscopeStickstoffbilanz(Base, Vector):
     the_geom = Column(Geometry2D)
 
 register(AgroscopeStickstoffbilanz.__bodId__, AgroscopeStickstoffbilanz)
+
+
+class AgroscopeTreibhausgasemissionen(Base, Vector):
+    __bodId__ = 'ch.agroscope.monitoring-agrarumweltsystem_treibhausgasemissionen'
+    __table_args__ = ({'schema': 'agroscope', 'autoload': False})
+    __tablename__ = 'monitoring_agrarumweltsystem_treibhausgasemissionen'
+    __template__ = 'templates/htmlpopup/monitoring-agrarumweltsystem_treibhausgasemissionen.mako'
+    __label__ = 'thg_emissionen_t_co2eq_ha'
+    __timeInstant__ = 'jahr'
+
+    id = Column('bgdi_id', Integer, primary_key=True)
+    thg_emissionen_t_co2eq_ha = Column('thg_emissionen_t_co2eq_ha', Float)
+    jahr = Column('jahr', Integer)
+    thg_emissionen_t_co2eq_ha_cat = Column('thg_emissionen_t_co2eq_ha_cat', Unicode)
+    the_geom = Column(Geometry2D)
+
+register(AgroscopeTreibhausgasemissionen.__bodId__, AgroscopeTreibhausgasemissionen)
